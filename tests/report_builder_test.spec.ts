a671da608ce73e4d75c3da3713daf8008dc17844
Diego Partida <dpartida@anterasoftware.com>
2026-01-21 08:52:33 -0600

    report builder test updated

--- tests/report_builder_test.spec.ts
+++ tests/report_builder_test.spec.ts
@@ -26,15 +26,44 @@ async function openMenu(page: Page): Promise<void> {
 	await menuButton.click();
 }
 
-
-test('report_builder_test', async ({ page }: { page: Page }) => {
-  // Login  
-  test.setTimeout(480000);
-  await login(page);
-	await openMenu(page);
-	const reportBuilderPage = await goToReportBuilder(page);
-	// Now interact with the Report Builder page
-	await reportBuilderPage.getByRole('img', { name: 'Sales', exact: true }).click();
+async function goToSalesReport(page: Page): Promise<Page> {
+	page.getByRole('img', { name: 'Sales', exact: true }).click();
 	await page.waitForTimeout(10000);
+	return page;
+}
 
+async function columnsSelectedCount(page: Page): Promise<number> {
+	const columnsSelectedElement = await page.getByText('columns selected');
+  const columnsSelectedText = await columnsSelectedElement.innerText();
+	const columnsSelectedCount = columnsSelectedText.split(' ')[0];
+	console.log(`Columns selected (from text): ${columnsSelectedCount}`);
+	const selectedColumnsElement = await page.getByRole('columnheader');
+	const selectedColumnsCount = await selectedColumnsElement.count();
+	console.log(`Columns selected (from count): ${selectedColumnsCount}`);
+
+	return selectedColumnsCount;
+	
+}
+
+test.describe('report builder suite', () => {
+	test.describe.configure({ timeout: 480000 });
+
+	test('report_builder_test', async ({ page }: { page: Page }) => {
+		// Login  
+		try {
+			await login(page);
+
+			await openMenu(page);
+
+			const reportBuilderPage = await goToReportBuilder(page);
+
+			const salesReportPage = await goToSalesReport(reportBuilderPage);
+
+			const columnsCount = await columnsSelectedCount(salesReportPage);
+			console.log(`Columns selected (final count): ${columnsCount}`);
+		} catch (error) {
+			console.error('Error during test execution:', error);
+			throw error;
+		}
+	});
 });
